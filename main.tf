81bc0f0a88bbf84d388015a6194b789581417c68
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-01 12:41:20 -0400

    Create a bucket with dynamic credentials

--- main.tf
+++ main.tf
@@ -1,38 +1,7 @@
-data "aws_ami" "rhel9" {
-  most_recent = true
+module "code_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~>4.6.0"
 
-  filter {
-    name   = "name"
-    values = ["RHEL-9*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  owners = ["309956199498"] # Red Hat
-}
-
-
-resource "aws_instance" "test" {
-  ami           = data.aws_ami.rhel9.id
-  instance_type = "t3.medium"
-  tags = { Name = "test",
-    owner = "nick",
-    foo   = "bar"
-  }
-  lifecycle {
-    ignore_changes = [ami]
-  }
+  bucket_prefix = "if-this-bucket-has-any-objects-in-it-its-your-ass"
+  force_destroy = true
 }
