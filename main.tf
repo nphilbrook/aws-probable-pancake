59068c1124faf9ebe26f04bd20b3a488b032e866
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-10 15:05:07 -0400

    Change name of S3 bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "code_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "~>4.6.0"
 
-  bucket_prefix = "any-objects-in-it-its-your-ass"
+  bucket_prefix = "no-objects-please"
   force_destroy = true
 }
+
