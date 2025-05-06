cbc795577ec801544689427b3a3098fbadbfd7d7
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-06 08:33:11 -0400

    some outputs, get the local in state

--- module_tags.tf
+++ module_tags.tf
@@ -3,9 +3,13 @@ data "local_file" "modules_json" {
 }
 
 locals {
-  modules = jsondecode(data.local_file.modules_json.content)
+  modules = jsondecode(data.local_file.modules_json.content)["Modules"]
 }
 
 output "modules" {
   value = local.modules
+}
+
+output "a_version" {
+  value = local.modules[1].Version
 }
