fb555b3f39e8e0be1fdd5238c7e40cc3007aeaf4
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-12 13:26:52 -0400

    yep, ditch this

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ module "code_bucket" {
   }
 }
 
-module "code_bucket_doormat" {
-  providers = {
-    aws = aws.doormat
-  }
+# module "code_bucket_doormat" {
+#   providers = {
+#     aws = aws.doormat
+#   }
 
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~>4.6.0"
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "~>4.6.0"
 
-  bucket_prefix = "no-objects-plz"
-  force_destroy = true
-}
+#   bucket_prefix = "no-objects-plz"
+#   force_destroy = true
+# }
