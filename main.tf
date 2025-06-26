5741b5f35d93f27cd1022c97f0a95657431a4d85
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-26 14:16:09 -0400

    Additional bucket in the other account

--- main.tf
+++ main.tf
@@ -14,14 +14,18 @@ module "code_bucket" {
   }
 }
 
-# module "code_bucket_doormat" {
-#   providers = {
-#     aws = aws.doormat
-#   }
+module "code_bucket_alternate" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~>4.6.0"
 
-#   source  = "terraform-aws-modules/s3-bucket/aws"
-#   version = "~>4.6.0"
+  providers = {
+    aws = aws.alternate
+  }
 
-#   bucket_prefix = "no-objects-plz"
-#   force_destroy = true
-# }
+  bucket_prefix = "${local.bucket_prefix_prefix}no-objects-plz"
+
+  force_destroy = true
+  tags = {
+    module_verison = local.verison_map["code_bucket"]
+  }
+}
